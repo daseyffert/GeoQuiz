d0d28a6322d7a61cc2973929447ed2b077126eaf
Daniel Seyffert <daseyffert@gmail.com>
2015-10-02 20:26:39 -0600

    Geo Quiz

---
+++ app/src/main/java/com/bignerdranch/android/geoquiz/Question.java
@@ -0,0 +1,31 @@
+package com.bignerdranch.android.geoquiz;
+
+/**
+ * Created by Daniel on 10/2/2015.
+ */
+public class Question {
+
+    private int mTextResId;
+    private  boolean mAnswerTrue;
+
+    public Question(int mTextResId, boolean mAnswerTrue){
+        this.mTextResId = mTextResId;
+        this.mAnswerTrue = mAnswerTrue;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+}
